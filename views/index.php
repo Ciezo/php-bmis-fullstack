6ee16eb5abe80ee2d237f355b87a9d993c8a5983
Cloyd Van <abcd123.letters@gmail.com>
2023-02-02 02:10:29 +0800

    Update to index page

--- views/index.php
+++ views/index.php
@@ -25,14 +25,10 @@ require("../database_instance.php");
         <link href="../css/index_home.css" rel="stylesheet">
         <!-- Bootstrap from https://getbootstrap.com/ -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     </head>
     <body class="bg-custom">
-	<header id="header" class="container mx-auto">
-		<figure id="img-div">
-			<br>
-			<figcaption id="img-caption" class="font-weight-bold display-4"> Information Management System </figcaption>
-		</figure>
-    </header>
         <!-- Top navbar at index ONLY -->
         <div class="index_navbar">
             <center>
@@ -42,8 +38,18 @@ require("../database_instance.php");
             </center>
         </div>
 
+        <center>
+            <div class="welcome-prompt">
+                <h1>Barangay Loma De Gato</h1>
+                <h5>News, Announcements, and Reporting Management System</h5>
+                <div class="logo">
+                    <img src="../assets/logo/ldg_logo.jpg" alt="Brngy. Loma De Gato logo">
+                </div>
+            </div>
+        </center>
+
     <section id="mantra" class="container bg-white text-center pt-4 pb-2">
-        <h2 class="font-weight-bold display-5 mx-auto">Mantra</h2>
+        <h2 class="font-weight-bold display-5 mx-auto">Marilao, Hymn</h2>
         <div class="content text-left p-4">
             <p>
             Masdan mo ang bayan ko <br>
@@ -78,8 +84,19 @@ require("../database_instance.php");
     </section>
 
     <section id="my-media" class="container bg-white text-center pt-4 pb-2 mx-auto">
-        <h2 class="font-weight-bold display-5 mx-auto">My Media</h2>
-        <div class="my-4 embed-responsive embed-responsive-16by9"><iframe height="100" width="250" class="embed-responsive-item" frameborder="0"></iframe></div>
+        <h2 class="font-weight-bold display-5 mx-auto">Social Media</h2>
+        <div class="mx-auto">
+            <a href="https://web.facebook.com/MarileNews" class="btn btn-block btn-social btn-facebook">
+                <span class="fa fa-facebook"></span>
+                    MarileNews
+            </a>
+            <br>
+            <a href="https://web.facebook.com/MarileNews" class="btn btn-block btn-social btn-facebook">
+                <span class="fa fa-facebook"></span>
+                    (Buy & Sell) Heritage Homes, Loma De Gato, Perez, Sto. Niño, Bahay pare
+            </a>
+
+        </div>
     </section>
 	
     <section id="contact" class="container bg-white text-center pt-4 pb-2 mx-auto">
@@ -99,9 +116,10 @@ require("../database_instance.php");
         </div>
     </section>
     
-    <footer id="foot" class="mt-0">
-        <p class="text-right">&copy; 2023 BIMS. All rights reserved.</p>
-    </footer>
-        
+    <center>
+        <footer id="foot" class="mt-0">
+            <p class="text-right">&copy; 2023 Loma De Gato, Marilao, Bulacan. All rights reserved.</p>
+        </footer>
+    </center>    
     </body>
 </html>
