a314ea633d1671064325d5d5ba0dcdb11df0250a
Cloyd Van <abcd123.letters@gmail.com>
2023-02-02 19:02:47 +0800

    Removed the bootstrap navbar at Login pages: - Resident - Admin
    PROBLEMS INCLUDED:
    1. If Bootstrap navbar is added
       Then, i simply cannot redirect to the home pages for Admin and Resident
    
    2. I do not know why this happens, or why it happens.
    3. I REALLY, REALLY TRIED FIXING THIS. I TESTED IT MULTIPLE TIMES.
        (a.) I remove the boot strap navbar, and use my custom navbar.   => Login and redirection works
        (b.) I put the Bootstrap navbar again, and now redirection to the Home pages do not work.
    
    4. I do not know.
    5. Therefore, I will only put the bootstrap navbar to the index.php

--- views/resident/resident_login_page.php
+++ views/resident/resident_login_page.php
@@ -24,72 +24,60 @@ session_start();
         <link href="../../css/index_navbar.css" rel="stylesheet">
         <!-- Bootstrap from https://getbootstrap.com/ -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-        <script src="https://kit.fontawesome.com/c36559a51c.js" crossorigin="anonymous"></script>
-        <style>
-        .navbar .navbar-brand {
-                padding-left: 30px;
-        }
-        .resident-login-form {
-          width: 30%;
-          padding: 30px;
-          background-color: #f2f2f2;
-          border-radius: 10px;
-          box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-          margin-top: 50px;
-        }
+    
+	<style>
+  .resident-login-form {
+    width: 30%;
+    padding: 30px;
+    background-color: #f2f2f2;
+    border-radius: 10px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    margin-top: 50px;
+  }
 
-        .input-fields {
-          width: 100%;
-          padding: 12px;
-          border-radius: 5px;
-          border: 1px solid #ccc;
-          margin-top: 6px;
-          margin-bottom: 16px;
-          resize: vertical;
-        }
+  .input-fields {
+    width: 100%;
+    padding: 12px;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+    margin-top: 6px;
+    margin-bottom: 16px;
+    resize: vertical;
+  }
 
-        .submit-form-btn {
-          width: 100%;
-          background-color: #4CAF50;
-          color: white;
-          padding: 12px 20px;
-          border-radius: 5px;
-          cursor: pointer;
-          border: none;
-          margin-top: 10px;
-        }
+  .submit-form-btn {
+    width: 100%;
+    background-color: #4CAF50;
+    color: white;
+    padding: 12px 20px;
+    border-radius: 5px;
+    cursor: pointer;
+    border: none;
+    margin-top: 10px;
+  }
 
-        .submit-form-btn:hover {
-          background-color: #45a049;
-          color: black;
-        }
+  .submit-form-btn:hover {
+    background-color: #45a049;
+    color: black;
+  }
 
-        h4 a {
-          color: #4CAF50;
-        }
-      </style>
+  h4 a {
+    color: #4CAF50;
+  }
+</style>
 
 	
 	</head>
     <body>
-        <!-- Bootstrap navbar -->
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="#">      
-            <i class="fa-solid fa-building-user"></i>
-                Barangay Loma De Gato 
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="true" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                <a class="nav-item nav-link" href="../index.php">Home</a>
-                <a class="nav-item nav-link active" href="#">Resident</a>
-                <a class="nav-item nav-link" href="../admin/admin_login_page.php">Admin</a>
-                </div>
-            </div>
-        </nav>
-
+        <!-- Top navbar at index ONLY -->
+        <div class="index_navbar">
+            <center>
+                <a href="../index.php">Home</a>
+                <a class="active" href="">Resident</a>
+                <a href="../admin/admin_login_page.php">Admin</a>
+            </center>
+        </div>
+        
         <center>
             <form action="resident_login_page.php" class="resident-login-form" method="POST">
                 <h1>Resident, Login</h1>
