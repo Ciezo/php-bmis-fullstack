d7ce8e8f5704a60e5e9c552edf4aa4bd8e42ae5f
Cloyd Van <abcd123.letters@gmail.com>
2023-02-03 04:18:05 +0800

    Updates to schema

--- sql/populate_tables.sql
+++
@@ -1,9 +0,0 @@
-
--- Load data about news category 
-INSERT INTO CATEGORY(category_name)
-    VALUES 
-        ('Road Blockage'),
-        ('Flood Updates'),
-        ('Crime'),
-        ('Recreation'),
-        ('Sports Event'); 
